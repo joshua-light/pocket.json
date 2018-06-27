0e75e6c894a3c467c3cd70e3f090244c56b82317
JoshuaLight <ashkariel.eter@gmail.com>
2018-06-27 12:35:51 -0700

    Refactored `Generate`.

--- src/Core/Core/Generate.cs
+++ src/Core/Core/Generate.cs
@@ -7,38 +7,28 @@ namespace Pocket.Json
 {
     internal static class Generate
     {
-        public static Append<object> Append(Type type)
-        {
-            return (Append<object>) typeof(AppendGenerator)
+        public static Append<object> Append(Type type) =>
+            (Append<object>) typeof(AppendGenerator)
                 .GetTypeInfo()
                 .GetDeclaredMethod("Generate")
                 .MakeGenericMethod(type)
                 .Invoke(null, null);
-        }
 
-        public static Unwrap<object> Unwrap(Type type)
-        {
-            return (Unwrap<object>) typeof(UnwrapGenerator)
+        public static Unwrap<object> Unwrap(Type type) =>
+            (Unwrap<object>) typeof(UnwrapGenerator)
                 .GetTypeInfo()
                 .GetDeclaredMethod("Generate")
                 .MakeGenericMethod(type)
                 .Invoke(null, null);
-        }
 
         internal class AppendGenerator
         {
-            public static Append<object> Generate<T>()
-            {
-                return (x, buffer) => Json<T>.Append((T) x, buffer);
-            }
+            public static Append<object> Generate<T>() => (x, buffer) => Json<T>.Append((T) x, buffer);
         }
 
         internal class UnwrapGenerator
         {
-            public static Unwrap<object> Generate<T>()
-            {
-                return x => Json<T>.Unwrap(x);
-            }
+            public static Unwrap<object> Generate<T>() => x => Json<T>.Unwrap(x);
         }
     }
 }
