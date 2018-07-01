0d4fffaec75d98a7ebe132978a6f6c7bb6902e74
JoshuaLight <ashkariel.eter@gmail.com>
2018-07-01 11:18:19 -0700

    Added `JsonCharTest`.

---
+++ src/Tests/Primitives/JsonCharTest.cs
@@ -0,0 +1,27 @@
+﻿using Xunit;
+
+namespace Pocket.Json.Tests.Primitives
+{
+    public class JsonCharTest : JsonTest
+    {
+        [Fact]
+        public void Append_ShouldWorkCorrectly()
+        {
+            Appends('a').As("a");
+            Appends('\r').As("\r");
+            Appends('\n').As("\n");
+            Appends('Ş').As("Ş");
+            Appends('ğ').As("ğ");
+        }
+
+        [Fact]
+        public void Unwrap_ShouldWorkCorrectly()
+        {
+            Unwraps("a").As('a');
+            Unwraps("\r").As('\r');
+            Unwraps("\n").As('\n');
+            Unwraps("Ş").As('Ş');
+            Unwraps("ğ").As('ğ');
+        }
+    }
+}
