2b1a53a7759dee698c043e68205b1c4c6870967c
JoshuaLight <ashkariel.eter@gmail.com>
2018-07-07 15:57:56 -0700

    Changed all cycles to `while` in `JsonStringSpan`.

--- src/Core/Core/JsonStringSpan.cs
+++ src/Core/Core/JsonStringSpan.cs
@@ -13,11 +13,10 @@
 
             var json = Json;
             var start = json.Offset;
-            var remainder = json.Length % 2;
-            var length = start + json.Length - remainder;
             var source = json.Source;
 
-            for (var i = start; i < length; i += 2)
+            var i = start;
+            while (true)
             {
                 var a = source[i];
                 if (a == '"')
@@ -26,16 +25,10 @@
                     span = json;
                     break;
                 }
-
-                var b = source[i + 1];
-                if (b == '"')
-                {
-                    json.Length = i - start + 1;
-                    span = json;
-                    break;
-                }
+                
+                i++;
             }
-
+            
             Json.SkipMutable(span.Length + 1);
 
             return true;
@@ -72,10 +65,10 @@
             var stack = 1;
             
             var start = json.Offset;
-            var length = start + json.Length - 1;
             var source = json.Source;
 
-            for (var i = start + 1; i < length; i++)
+            var i = start + 1;
+            while (true)
             {
                 var ch = source[i];
                 if (ch == '{')
@@ -85,6 +78,8 @@
                     json = json.SubSpan(i - start + 1);
                     break;
                 }
+                
+                i++;
             }
         }
         
@@ -94,43 +89,46 @@
             var stack = 1;
             
             var start = json.Offset;
-            var length = start + json.Length - 1;
             var source = json.Source;
 
-            for (var i = start + 1; i < length; i++)
+            var i = start + 1;
+            while (true)
             {
                 var ch = source[i];
-
-                stack += Bitwise.Equals(ch, '[');
-                stack -= Bitwise.Equals(ch, ']');
-                
-                if (stack == 0)
+                if (ch == '[')
+                    stack++;
+                else if (ch == ']' && --stack == 0)
                 {
                     json = json.SubSpan(i - start + 1);
                     break;
                 }
+                
+                i++;
             }
         }
 
         public void NextString(ref StringSpan json)
         {
-            for (var i = 1; i < json.Length; i++)
+            var i = 1;
+            while (true)
             {
-                if (json[i] != '"')
-                    continue;
+                if (json[i] == '"')
+                {
+                    json = json.SubSpan(i + 1);
+                    break;
+                }
 
-                json = json.SubSpan(i + 1);
-                break;
+                i++;
             }
         }
 
         public void NextPrimitive(ref StringSpan json)
         {
             var start = json.Offset;
-            var length = start + json.Length;
             var source = json.Source;
-
-            for (var i = start; i < length; i++)
+            
+            var i = start;
+            while (true)
             {
                 var a = source[i];
                 if (a == ',' || a == ':')
@@ -138,6 +136,8 @@
                     json.Length = i - start;
                     break;
                 }
+                
+                i++;
             }
         }
     }
