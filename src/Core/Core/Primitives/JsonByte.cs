d7c5bf1e1650ebf5462cd2835c836eab6b90be66
JoshuaLight <ashkariel.eter@gmail.com>
2018-07-12 13:24:59 -0700

    Optimized variables usage in `JsonByte`.

--- src/Core/Core/Primitives/JsonByte.cs
+++ src/Core/Core/Primitives/JsonByte.cs
@@ -11,16 +11,19 @@
 
         public static byte Unwrap(StringSpan json)
         {
+            var source = json.Source;
+            var offset = json.Offset;
+            
             if (json.Length == 1)
-                return (byte) (json.Source[json.Offset + 0] - '0');
+                return (byte) (source[offset + 0] - '0');
 
             if (json.Length == 2)
-                return (byte) ((json.Source[json.Offset + 0] - '0') * 10
-                    + (json.Source[json.Offset + 1] - '0'));
+                return (byte) ((source[offset + 0] - '0') * 10
+                    + (source[offset + 1] - '0'));
 
-            return (byte) ((json.Source[json.Offset + 0] - '0') * 100
-                + (json.Source[json.Offset + 1] - '0') * 10
-                + (json.Source[json.Offset + 2] - '0'));
+            return (byte) ((source[offset + 0] - '0') * 100
+                + (source[offset + 1] - '0') * 10
+                + (source[offset + 2] - '0'));
         }
     }
 }
