df15e0ff7bbaf8113d3c844bfef24884d5268de0
JoshuaLight <ashkariel.eter@gmail.com>
2018-07-12 13:19:51 -0700

    Optimized variables usage in `JsonInt`.

--- src/Core/Core/Primitives/JsonInt.cs
+++ src/Core/Core/Primitives/JsonInt.cs
@@ -11,9 +11,11 @@
 
         public static int Unwrap(StringSpan json)
         {
+            var source = json.Source;
+            var offset = json.Offset;
+            
             var multiplier = 1;
-
-            if (json.Source[json.Offset + 0] == '-')
+            if (source[offset + 0] == '-')
             {
                 multiplier = -1;
                 json.SkipMutable(1);
@@ -23,79 +25,79 @@
             switch (json.Length)
             {
                 case 1:
-                    value = json.Source[json.Offset + 0] - '0';
+                    value = source[offset + 0] - '0';
                     break;
                 case 2:
-                    value = (json.Source[json.Offset + 0] - '0') * 10
-                        + (json.Source[json.Offset + 1] - '0');
+                    value = (source[offset + 0] - '0') * 10
+                        + (source[offset + 1] - '0');
                     break;
                 case 3:
-                    value = (json.Source[json.Offset + 0] - '0') * 100
-                        + (json.Source[json.Offset + 1] - '0') * 10
-                        + (json.Source[json.Offset + 2] - '0');
+                    value = (source[offset + 0] - '0') * 100
+                        + (source[offset + 1] - '0') * 10
+                        + (source[offset + 2] - '0');
                     break;
                 case 4:
-                    value = (json.Source[json.Offset + 0] - '0') * 1000
-                        + (json.Source[json.Offset + 1] - '0') * 100
-                        + (json.Source[json.Offset + 2] - '0') * 10
-                        + (json.Source[json.Offset + 3] - '0');
+                    value = (source[offset + 0] - '0') * 1000
+                        + (source[offset + 1] - '0') * 100
+                        + (source[offset + 2] - '0') * 10
+                        + (source[offset + 3] - '0');
                     break;
                 case 5:
-                    value = (json.Source[json.Offset + 0] - '0') * 10000
-                        + (json.Source[json.Offset + 1] - '0') * 1000
-                        + (json.Source[json.Offset + 2] - '0') * 100
-                        + (json.Source[json.Offset + 3] - '0') * 10
-                        + (json.Source[json.Offset + 4] - '0');
+                    value = (source[offset + 0] - '0') * 10000
+                        + (source[offset + 1] - '0') * 1000
+                        + (source[offset + 2] - '0') * 100
+                        + (source[offset + 3] - '0') * 10
+                        + (source[offset + 4] - '0');
                     break;
                 case 6:
-                    value = (json.Source[json.Offset + 0] - '0') * 100000
-                        + (json.Source[json.Offset + 1] - '0') * 10000
-                        + (json.Source[json.Offset + 2] - '0') * 1000
-                        + (json.Source[json.Offset + 3] - '0') * 100
-                        + (json.Source[json.Offset + 4] - '0') * 10
-                        + (json.Source[json.Offset + 5] - '0');
+                    value = (source[offset + 0] - '0') * 100000
+                        + (source[offset + 1] - '0') * 10000
+                        + (source[offset + 2] - '0') * 1000
+                        + (source[offset + 3] - '0') * 100
+                        + (source[offset + 4] - '0') * 10
+                        + (source[offset + 5] - '0');
                     break;
                 case 7:
-                    value = (json.Source[json.Offset + 0] - '0') * 1000000
-                        + (json.Source[json.Offset + 1] - '0') * 100000
-                        + (json.Source[json.Offset + 2] - '0') * 10000
-                        + (json.Source[json.Offset + 3] - '0') * 1000
-                        + (json.Source[json.Offset + 4] - '0') * 100
-                        + (json.Source[json.Offset + 5] - '0') * 10
-                        + (json.Source[json.Offset + 6] - '0');
+                    value = (source[offset + 0] - '0') * 1000000
+                        + (source[offset + 1] - '0') * 100000
+                        + (source[offset + 2] - '0') * 10000
+                        + (source[offset + 3] - '0') * 1000
+                        + (source[offset + 4] - '0') * 100
+                        + (source[offset + 5] - '0') * 10
+                        + (source[offset + 6] - '0');
                     break;
                 case 8:
-                    value = (json.Source[json.Offset + 0] - '0') * 10000000
-                        + (json.Source[json.Offset + 1] - '0') * 1000000
-                        + (json.Source[json.Offset + 2] - '0') * 100000
-                        + (json.Source[json.Offset + 3] - '0') * 10000
-                        + (json.Source[json.Offset + 4] - '0') * 1000
-                        + (json.Source[json.Offset + 5] - '0') * 100
-                        + (json.Source[json.Offset + 6] - '0') * 10
-                        + (json.Source[json.Offset + 7] - '0');
+                    value = (source[offset + 0] - '0') * 10000000
+                        + (source[offset + 1] - '0') * 1000000
+                        + (source[offset + 2] - '0') * 100000
+                        + (source[offset + 3] - '0') * 10000
+                        + (source[offset + 4] - '0') * 1000
+                        + (source[offset + 5] - '0') * 100
+                        + (source[offset + 6] - '0') * 10
+                        + (source[offset + 7] - '0');
                     break;
                 case 9:
-                    value = (json.Source[json.Offset + 0] - '0') * 100000000
-                        + (json.Source[json.Offset + 1] - '0') * 10000000
-                        + (json.Source[json.Offset + 2] - '0') * 1000000
-                        + (json.Source[json.Offset + 3] - '0') * 100000
-                        + (json.Source[json.Offset + 4] - '0') * 10000
-                        + (json.Source[json.Offset + 5] - '0') * 1000
-                        + (json.Source[json.Offset + 6] - '0') * 100
-                        + (json.Source[json.Offset + 7] - '0') * 10
-                        + (json.Source[json.Offset + 8] - '0');
+                    value = (source[offset + 0] - '0') * 100000000
+                        + (source[offset + 1] - '0') * 10000000
+                        + (source[offset + 2] - '0') * 1000000
+                        + (source[offset + 3] - '0') * 100000
+                        + (source[offset + 4] - '0') * 10000
+                        + (source[offset + 5] - '0') * 1000
+                        + (source[offset + 6] - '0') * 100
+                        + (source[offset + 7] - '0') * 10
+                        + (source[offset + 8] - '0');
                     break;
                 default:
-                    value = (json.Source[json.Offset + 0] - '0') * 1000000000
-                        + (json.Source[json.Offset + 1] - '0') * 100000000
-                        + (json.Source[json.Offset + 2] - '0') * 10000000
-                        + (json.Source[json.Offset + 3] - '0') * 1000000
-                        + (json.Source[json.Offset + 4] - '0') * 100000
-                        + (json.Source[json.Offset + 5] - '0') * 10000
-                        + (json.Source[json.Offset + 6] - '0') * 1000
-                        + (json.Source[json.Offset + 7] - '0') * 100
-                        + (json.Source[json.Offset + 8] - '0') * 10
-                        + (json.Source[json.Offset + 9] - '0');
+                    value = (source[offset + 0] - '0') * 1000000000
+                        + (source[offset + 1] - '0') * 100000000
+                        + (source[offset + 2] - '0') * 10000000
+                        + (source[offset + 3] - '0') * 1000000
+                        + (source[offset + 4] - '0') * 100000
+                        + (source[offset + 5] - '0') * 10000
+                        + (source[offset + 6] - '0') * 1000
+                        + (source[offset + 7] - '0') * 100
+                        + (source[offset + 8] - '0') * 10
+                        + (source[offset + 9] - '0');
                     break;
             }
 
