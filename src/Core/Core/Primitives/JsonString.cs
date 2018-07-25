8c94c3b0c58edc9b7f30c1bae48538cb78d57fb5
JoshuaLight <ashkariel.eter@gmail.com>
2018-07-25 14:49:41 -0700

    Removed redundant validations from `JsonString`.

--- src/Core/Core/Primitives/JsonString.cs
+++ src/Core/Core/Primitives/JsonString.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace Pocket.Json
+﻿namespace Pocket.Json
 {
     internal static class JsonString
     {
@@ -12,16 +10,6 @@ namespace Pocket.Json
         public static string Unwrap(JsonSpan json)
         {
             var span = json.NextString();
-            var start = span.Start;
-            var end = span.End;
-
-            if (start == end)
-                throw new ArgumentException("Cannot unwrap empty json to string", nameof(span));
-            if (end == 1)
-                throw new ArgumentException("Cannot unwrap single character json to string", nameof(span));
-            if (span.Source[start] != '"' || span.Source[end - 1] != '"')
-                throw new ArgumentException($"Specified string \"{span}\" must have open and close quotes characters.", nameof(span));
-            
             span.Start++;
             span.End--;
             
